9a90657920d029550bbf2c9a6a6e1c1a165228e3
Danielle Hobbs <daniellekristinehobbs@gmail.com>
2018-11-29 16:45:10 -0800

    start header and footer

--- inc/widgets.php
+++ inc/widgets.php
@@ -14,5 +14,33 @@ function stanelywp_widgets_init() {
 		'before_title'  => '<h3 class="widget-title card-header">',
 		'after_title'   => '</h3>',
 	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer 1', 'stanelywp' ),
+		'id'            => 'footer-1',
+		'description'   => esc_html__( 'Add widgets here.', 'stanelywp' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer 2', 'stanelywp' ),
+		'id'            => 'footer-2',
+		'description'   => esc_html__( 'Add widgets here.', 'stanelywp' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer 3', 'stanelywp' ),
+		'id'            => 'footer-3',
+		'description'   => esc_html__( 'Add widgets here.', 'stanelywp' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
 }
 add_action( 'widgets_init', 'stanelywp_widgets_init' );
+
